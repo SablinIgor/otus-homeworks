73d444d1b89deff31ba973a93f3ba15f73a5a45c
SablinIgor <sablin.igor@gmail.com>
2019-04-20 13:40:02 +0300

    fix

--- express42/run.sh
+++ express42/run.sh
@@ -8,17 +8,14 @@ echo "Clone repository with tests"
 git clone https://github.com/SablinIgor/otus-homeworks.git
 
 if [ -f $HOMEWORK_RUN ]; then
-	echo "Run tests"
-
+	echo "Run tests with docker"
     # Prepare network & run container
 	docker network create hw-test-net
     docker run -d -v $(pwd):/srv -v /var/run/docker.sock:/tmp/docker.sock \
 		-e DOCKER_HOST=unix:///tmp/docker.sock --cap-add=NET_ADMIN -p 33433:22 --privileged \
 		--device /dev/net/tun --name hw-test --network hw-test-net $DOCKER_IMAGE
-
     # Show versions & run tests
     docker exec hw-test $HOMEWORK_RUN
-
 else
 	echo "We don't have tests for this homework"
 	exit 0
