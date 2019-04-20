503f4ca9f2e2ab6772b43b8b2cea846970bf75e4
SablinIgor <sablin.igor@gmail.com>
2019-04-20 13:24:23 +0300

    Run ansible check

--- express42/run.sh
+++ express42/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
+HOMEWORK_RUN=./otus-homeworks/express42/ansible/run.sh
 DOCKER_IMAGE=express42/otus-homeworks
 
 echo "Clone repository with tests"
+git clone https://github.com/SablinIgor/otus-homeworks.git
+
+if [ -f $HOMEWORK_RUN ]; then
+	echo "Run tests"
+else
+	echo "We don't have tests for this homework"
+	exit 0
+fi
